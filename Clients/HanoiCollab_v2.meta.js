1f21d557a815869c76b91c4c59909c7134665563
dattranbenit <dattran.benitl@gmail.com>
2022-05-26 20:21:58 +0700

    commit

---
+++ Clients/HanoiCollab_v2.meta.js
@@ -0,0 +1,4 @@
+// ==UserScript==
+// @name         HanoiCollab_v2
+// @version      0.2.5
+// ==/UserScript==
---
+++ HanoiCollab_v2/Models/DatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace HanoiCollab.Models
+
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+
+        public string DatabaseName { get; set; }
+
+        public string UsersCollectionName { get; set; }
+
+        public string QuestionsCollectionName { get; set; }
+    }
+}
